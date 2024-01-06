5929e790a1feac513164371ac528cf2136162d31
K Koyal <101511737+kondapalli19@users.noreply.github.com>
2024-01-06 17:17:11 +0530

    Update intro.tsx

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -47,7 +47,7 @@ export default function Intro() {
         <motion.h1 className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}>
-        <span className="font-bold">Hello, I'm K Koyal,{" "}</span> 
+        <span className="font-bold">Hi, I'm K Koyal,{" "}</span> 
         <span className="font-medium">final-year B.Tech student </span> passionate about coding in{" "}
         <span className="font-bold">full-stack development.</span> I enjoy
         building <span className="italic">sites & apps</span> with a focus on{" "}
